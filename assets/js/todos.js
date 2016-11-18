49bcfe9c582b5da2a238334983e56f814b7666dd
Ashish <ashishrodio@gmail.com>
2016-11-18 18:35:04 +0530

    Finished Prog Logic - Styling Pending

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,22 @@
-//For Custom JS Overrides
+//Check Of Specific Todo's By Clicking
+$("ul").on('click','li',function () {
+    $(this).toggleClass("completed");
+});
+
+//Delete Todo's
+$("ul").on("click","span",function (event) {
+    console.log("out :" +this);
+    $(this).parent().fadeOut(500,function () {
+        console.log("In :"+this);
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+//text Field
+$("input[type='text']").keypress(function (event) {
+    if (event.which == 13) {
+        var todoName = $(this).val();
+        $("ul").append("<li><span>X</span> "+todoName+"</li>");
+    }
+});
