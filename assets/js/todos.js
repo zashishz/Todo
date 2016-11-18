fd906a892c5c7d6e10bbee05cbdba04dff8c7e83
Ashish <ashishrodio@gmail.com>
2016-11-18 19:47:22 +0530

    Full Working Sexy Todo List

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -17,6 +17,12 @@ $("ul").on("click","span",function (event) {
 $("input[type='text']").keypress(function (event) {
     if (event.which == 13) {
         var todoName = $(this).val();
-        $("ul").append("<li><span>X</span> "+todoName+"</li>");
+        $(this).val("");
+        $("ul").append('<li><span><i class="fa fa-trash"></i></span> '+todoName+'</li>');
     }
+});
+
+//Add New Plus icon
+$(".fa-plus").click(function () {
+    $("input[type='text']").fadeToggle();
 });
